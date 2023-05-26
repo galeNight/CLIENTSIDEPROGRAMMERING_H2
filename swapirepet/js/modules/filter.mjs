75577287dbf9e823ecd5661c98977571421bff3c
galeNight <97892095+galeNight@users.noreply.github.com>
2023-05-26 14:08:50 +0200

    Add files via upload

---
+++ swapirepet/js/modules/filter.mjs
@@ -0,0 +1,16 @@
+const ignore = [
+    "films",
+    "vehicles",
+    "starships",
+    "created",
+    "edited",
+    "people",
+    "homeworld",
+    "species",
+    "url",
+    "residents",
+    "characters",
+    "planets",
+    "pilots"
+]
+export{ignore};
--- swapirepet/js/script.js
+++ swapirepet/js/script.js
@@ -1,8 +1,9 @@
-import { ignore } from "./modules/filter.js";  
+import { ignore } from "./modules/filter.mjs";  //imports a const from a nother skript and usses them
 const swapiapp = (async function(){
     const SWAPIURL = "https://swapi.dev/api"
     const navBar = document.querySelector(".navbar")
     const cardcontainer = document.querySelector(".cardcontainer")
+
     try{
         const response = await fetch(SWAPIURL)
         const jsonData = await response.json();
@@ -14,33 +15,61 @@ const swapiapp = (async function(){
             navItem.href=jsonData[key];
             navBar.appendChild(navItem);
         }
+        document.querySelectorAll(".navItem")[0].click();
     }
     catch(error){
         console.log(error);
     }
     async function navClick(e){
         e.preventDefault();
+        document.querySelector(".active")?.classList.remove("active");
+        this.classList.add("active")
         let Data = await GetData(this.href)
-        console.log(this.href);
-
-        Data.results.forEach(DataItem =>{
+        showData(Data);
+    }
+    //mark area you wish to add to a new function showtcut ctrl+shif+r
+    function showData(Data) {
+        cardcontainer.innerHTML="";
+        document.querySelector("#pageing").innerHTML="";
+        if(Data.previous!=null){
+            const btnprev=document.createElement("a")
+            btnprev.addEventListener("click",Page);
+            btnprev.innerText="< prev";
+            btnprev.href=Data.previous
+            document.querySelector("#pageing").appendChild(btnprev);
+            //opret knab i pagining
+        }
+        if(Data.next !=null){
+            ////opret knab pagining
+            const btnNext=document.createElement("a")
+            btnNext.addEventListener("click",Page);
+            btnNext.innerText="Next >";
+            btnNext.href=Data.next;
+            document.querySelector("#pageing").appendChild(btnNext);
+        }
+        Data.results.forEach(DataItem => {
             let card = document.createElement("div");
-            card.className="card"
+            card.className = "card";
             // card.innerText=DataItem.name;
-            for(let[k,v] of Object.entries(DataItem)){
-                if(ignore.includes(k)){continue}
-                card.insertAdjacentHTML("beforeend",`<span class="key">${k.replace( "_", " ")}</span><span class="val">${v}</span><br>`)
+            for (let [k, v] of Object.entries(DataItem)) {
+                if (ignore.includes(k)) { continue; }
+                card.insertAdjacentHTML("beforeend", `<span class="key">${k.replace("_", " ")}</span>: <span class="val">${v}</span><br>`);
             }
-            cardcontainer.appendChild(card); 
-        })
-        // console.log(Data)
+            cardcontainer.appendChild(card);
+        });
+    }
+    async function Page(e){//pageurldata
+        e.preventDefault();
+        // alert(this.href);
+        const Data=await GetData(this.href);
+        showData(Data)
     }
     async function GetData(url){
         const response = await fetch(url)
         return await response.json();
     }
 })();
-
+//clock timer for array img swap every 1.6 min count in milisec
 document.addEventListener("DOMContentLoaded",function(Event){
    
     var imgSource = ["../img/andromedaGalaxy.jpg","../img/NebulaPink.jpg","../Nebulacolor.jpg"]
