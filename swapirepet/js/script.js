6e3e0794206ad8c5fcc5f7227e7bb521619c8a5d
galeNight <97892095+galeNight@users.noreply.github.com>
2023-05-31 09:34:06 +0200

    Add files via upload

--- swapirepet/js/script.js
+++ swapirepet/js/script.js
@@ -1,43 +1,55 @@
-import { ignore,ignore2 } from "./modules/filter.mjs";  //imports a const from a nother skript and usses them
-
+//import constants from another script
+import { ignore,ignore2 } from "./modules/filter.mjs";  
 
 const swapiapp = (async function(){
     const SWAPIURL = "https://swapi.dev/api"
     const navBar = document.querySelector(".navbar")
     const cardcontainer = document.querySelector(".cardcontainer")
     const pageing = document.querySelector("#pageing")
+    let activeNavItemIndex=0;
 
-    try{ // metod to get and await data
+    try{
+        //method to get and await data from SWAPIURL
         const response = await fetch(SWAPIURL)
         const jsonData = await response.json();
 
+        //interate over the properties of the json
         for(let key in jsonData){
             let navItem = document.createElement("a");
             navItem.addEventListener("click",navClick)
             navItem.className="navItem"
             navItem.innerText=key;
             navItem.href=jsonData[key];
+            //create and add navigation item to the navbar
             navBar.appendChild(navItem);
         }
+        //trigger a click event on the first navigaton item
         document.querySelectorAll(".navItem")[0].click();
     }
-    catch(error){
+    // catch eny error and show it in console log
+    catch(error){ 
         console.log(error);
     }
-
-    async function navClick(e){ // create a click and activate funtion on navbar 
+    async function navClick(e){ 
+        //event handler for navigation item clicks
         e.preventDefault();
+        //add the active clas to the clicked navigation item
+        // and removes it from det prevuis click item
         document.querySelector(".active")?.classList.remove("active");
         this.classList.add("active")
+        activeNavItemIndex=Array.from(navBar.children).indexOf(this)
         let Data = await GetData(this.href)
+        //fetch and display data for the selected category
         showData(Data);
     }
     
-    function showData(Data) { // display api data in a grid
+    function showData(Data) {
+        //display data in card container
         cardcontainer.style.display="grid"
         cardcontainer.innerHTML="";
         document.querySelector("#pageing").innerHTML="";
         
+        //add pagination buttons if previous and/or nex page are available
         if(Data.previous != null){
             const btnprev=document.createElement("a")
             btnprev.addEventListener("click",Page);
@@ -54,35 +66,61 @@ const swapiapp = (async function(){
             document.querySelector("#pageing").appendChild(btnNext);
         }
 
+        //interrate over the resulted and create card element
         Data.results.forEach(DataItem => { 
             let card = document.createElement("div");
             card.className = "card";
             for (let [k, v] of Object.entries(DataItem)) {
-                if (ignore.includes(k)) {continue;}// removes specific data from the json object
+                //skip ignored properties
+                if (ignore.includes(k)) {continue;}
+                //display key-values pairs in the card
                 card.insertAdjacentHTML("beforeend", `<span class="key">${k.replace("_", " ")}</span>: <span class="val">${v}</span><br>`);
             }
             card.addEventListener("click",()=>{
                 showCardInfo(DataItem);
             });
+            //add the card to the card container
             cardcontainer.appendChild(card);
         });
     }
 
-    async function Page(e){ // create a function to a new page
+    async function Page(e){
+        // Event handler for pagination buttons
         e.preventDefault();
         const Data=await GetData(this.href);
+        // Fetch and display data for the selected page
         showData(Data)
     }
 
-    function showCardInfo(DataItem){ // show card Info when is click on
+    function showCardInfo(DataItem){ 
+          // Display detailed information for a card item
         cardcontainer.style.display="block"
         cardcontainer.innerHTML ="";
         const card =document.createElement("div");
         card.className ="active card"
-
+      
+        const bactbtncon=document.createElement("div");
+        bactbtncon.className="bactbtncon";
+        const btnBack=document.createElement("button");
+        btnBack.className="btnBack"
+        btnBack.innerText ="X";
+        btnBack.addEventListener("click",()=>{
+             //add the active clas to the clicked navigation item
+             // and removes it from det prevuis click item
+             // and go back to the active navbar click via activenavitemindex
+            document.querySelector(".active")?.classList.remove("active")
+            if(activeNavItemIndex>=0&&activeNavItemIndex<navBar.children.length){
+                navBar.children[activeNavItemIndex].click();
+            }
+        });
+        bactbtncon.appendChild(btnBack)
+        card.appendChild(bactbtncon)
+       // Iterate over the properties of the data item
         for(let [k,v] of Object.entries(DataItem)){
             if(!Array.isArray(v)){
+                // Skip ignored properties
                 if(ignore2.includes(k)){ continue}
+                 // Display key-value pairs in the card
                 card.insertAdjacentHTML("beforeend", `<span class="key">${k.replace("_", " ")}</span>: <span class="val">${v}</span><br>`);
             }
             else{
@@ -101,18 +139,21 @@ const swapiapp = (async function(){
                     listcon.appendChild(inList)
                     list.appendChild(listcon)
                 }
+                // Display nested list in the card
                 card.appendChild(list)
             }
         }
         cardcontainer.appendChild(card);
-        //hide the prev and nex button when click on a card
+
+        //hide btnprev and/or btnnext
         const btnprev = pageing.querySelector("a:first-child")
         const btnNext = pageing.querySelector("a:last-child")
         btnprev.style.display = "none"
         btnNext.style.display = "none"
     }
 
-    async function GetData(url){ // get data from a url
+    async function GetData(url){
+           // Fetch data from a URL
         const response = await fetch(url)
         return await response.json();
         
@@ -121,8 +162,7 @@ const swapiapp = (async function(){
 })();
 
 
-//clock timer for array img swap every 1.6 min count in milisec
-
+// //clock timer for array img swap every 1.6 min count in milisec
 // document.addEventListener("DOMContentLoaded",function(Event){
    
 //     var imgSource = ["../img/andromedaGalaxy.jpg","../img/NebulaPink.jpg","../Nebulacolor.jpg"]
