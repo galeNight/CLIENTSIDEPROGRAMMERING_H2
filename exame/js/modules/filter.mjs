63e3fa9ec2743b780804d2f8bd23e571235a6a08
galeNight <97892095+galeNight@users.noreply.github.com>
2023-05-31 09:44:31 +0200

    Add files via upload

---
+++ exame/js/modules/filter.mjs
@@ -0,0 +1,21 @@
+const ignore = [
+    "films",
+    "vehicles",
+    "starships",
+    "created",
+    "edited",
+    "people",
+    "homeworld",
+    "species",
+    "url",
+    "residents",
+    "characters",
+    "planets",
+    "pilots"
+]
+const ignore2 = [
+    "created",
+    "edited",
+    "url",
+]
+export{ignore, ignore2};
